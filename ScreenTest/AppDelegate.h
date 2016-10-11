66314dd4911732b8eabd952afb38ff5ee52274d4
luyu <luyu@localhost>
2016-10-11 11:18:44 +0800

    Initial Commit

---
+++ ScreenTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScreenTest
+//
+//  Created by luyu on 16/10/11.
+//  Copyright © 2016年 luyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScreenTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScreenTest
+//
+//  Created by luyu on 16/10/11.
+//  Copyright © 2016年 luyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
